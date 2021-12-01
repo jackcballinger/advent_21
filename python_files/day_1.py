d570440b4f5a8537d07df9daa189740ac8c9c576
Jack Ballinger <42001353+jackcballinger@users.noreply.github.com>
2021-12-01 10:11:56 +0000

    commenting answers

--- python_files/day_1.py
+++ python_files/day_1.py
@@ -5,6 +5,8 @@ with open(Path(__file__).parent.parent / 'input_data' / 'day_1.txt') as f:
 
 # part 1
 part_1 = sum([(data[i+1]-data[i])>0 for i in range(len(data)-1)])
+# 1121
 
 # part 2
 part_2 = sum([(sum([data[i], data[i-1], data[i-2]])-sum([data[i-1], data[i-2], data[i-3]]))>0  for i in range(3, len(data))])
+# 1065
