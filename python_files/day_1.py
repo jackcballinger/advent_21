11e000af3ecca39df50ec10ebf91817ec130c8f1
Jack Ballinger <42001353+jackcballinger@users.noreply.github.com>
2021-12-01 10:10:37 +0000

    day_1

---
+++ python_files/day_1.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+with open(Path(__file__).parent.parent / 'input_data' / 'day_1.txt') as f:
+    data = [int(x) for x in f.read().splitlines()]
+
+# part 1
+part_1 = sum([(data[i+1]-data[i])>0 for i in range(len(data)-1)])
+
+# part 2
+part_2 = sum([(sum([data[i], data[i-1], data[i-2]])-sum([data[i-1], data[i-2], data[i-3]]))>0  for i in range(3, len(data))])
